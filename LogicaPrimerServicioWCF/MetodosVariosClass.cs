383b0f23dc8ca1e26c563abd06f5849b952ac3cb
Steven <steven.cadena.giler@gmail.com>
2022-02-22 10:40:13 +0100

    Add project files.

---
+++ LogicaPrimerServicioWCF/MetodosVariosClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPrimerServicioWCF
+{
+    //importante poner la clase publica 
+    public class MetodosVariosClass : IMetodosVariosContract
+    {
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Mi primer WCF de martes, " + nombre;
+        }
+
+        public string MetodoVisible()
+        {
+            return "no valgo para el servicio";
+        }
+    }
+}
